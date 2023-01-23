12526623bf2b7820c5e4087670ecc492896b3ac5
Taylor T Hight <Taylorthight@gmail.com>
2023-01-23 07:41:08 -0900

    addition of click event listeners to buttons

--- Calculator.js
+++ Calculator.js
@@ -1,19 +1,63 @@
-const one = document.getElementById('.one');
-const two = document.getElementById('.two');
-const three = document.getElementById('.three');
-const four = document.getElementById('.four');
-const five = document.getElementById('.five');
-const six = document.getElementById('.six');
-const seven = document.getElementById('.seven');
-const eight = document.getElementById('.eight');
-const nine = document.getElementById('.nine');
-const zero = document.getElementById('.zero');
-const allClear = document.getElementById('.all-clear');
-const clear = document.getElementById('.clear');
-const percent = document.getElementById('.percent');
-const divide = document.getElementById('.divide');
-const multiply = document.getElementById('.multiply');
-const minus = document.getElementById('.minus');
-const plus = document.getElementById('.plus');
-const decimal = document.getElementById('.decimal');
-const equal = document.getElementById('.equal');
+const one = document.getElementById('one').addEventListener('click', function(){
+    alert('click')
+});
+const two = document.getElementById('two').addEventListener('click', function(){
+    alert('click')
+});
+const three = document.getElementById('three').addEventListener('click', function(){
+    alert('click')
+});
+const four = document.getElementById('four').addEventListener('click', function(){
+    alert('click')
+});
+const five = document.getElementById('five').addEventListener('click', function(){
+    alert('click')
+});
+const six = document.getElementById('six').addEventListener('click', function(){
+    alert('click')
+});
+const seven = document.getElementById('seven').addEventListener('click', function(){
+    alert('click')
+});
+const eight = document.getElementById('eight').addEventListener('click', function(){
+    alert('click')
+});
+const nine = document.getElementById('nine').addEventListener('click', function(){
+    alert('click')
+});
+const zero = document.getElementById('zero').addEventListener('click', function(){
+    alert('click')
+});
+const allClear = document.getElementById('all-clear').addEventListener('click', function(){
+    alert('click')
+});
+const clear = document.getElementById('clear').addEventListener('click', function(){
+    alert('click')
+});
+const percent = document.getElementById('percent').addEventListener('click', function(){
+    alert('click')
+});
+const divide = document.getElementById('divide').addEventListener('click', function(){
+    alert('click')
+});
+const multiply = document.getElementById('multiply').addEventListener('click', function(){
+    alert('click')
+});
+const minus = document.getElementById('minus').addEventListener('click', function(){
+    alert('click')
+});
+const plus = document.getElementById('plus').addEventListener('click', function(){
+    alert('click')
+});
+const decimal = document.getElementById('decimal').addEventListener('click', function(){
+    alert('click')
+});
+const equal = document.getElementById('equal').addEventListener('click', function(){
+    alert('click')
+});
+
+//assign numerical values to the number buttons
+
+//assign mathematical functions to the symbol buttons
+
+//Create function for displaying the numerical values of the buttons on the display
