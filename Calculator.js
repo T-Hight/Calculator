ca629cd8d4d4b3b7fbaa38a8ee2716a78a37114e
Taylor T Hight <Taylorthight@gmail.com>
2023-01-30 07:54:32 -0900

    create variables to store selected buttons

--- Calculator.js
+++ Calculator.js
@@ -1,3 +1,11 @@
+//Create function for displaying the numerical values of the buttons on the display
+const numbers = document.querySelectorAll('.number');
+const operator = document.querySelectorAll('.operator');
+const currentOperand = document.querySelector('.current-operand');
+const previousOperand = document.querySelector('.previous-operand');
+const del = document.getElementById('clear');
+const allClear = document.getElementById('all-clear')
+
 //Addition
 const add = function(a, b, ...args) {
     let total = a + b;
@@ -36,20 +44,43 @@ const div = function(a, b, ...args) {
 
 //Create a new function operate that takes an operator and 2 numbers 
 //and then calls one of the above functions on the numbers.
-const operate = function() {
-    
-}
+const operate = function(a, b, c) {
+    let output = 0;
+  
+    try {
+  
+        switch(b) {
+            case '+':
+                output = add(a, c)
+                break;
+  
+            case '*':
+                output = mult(a, c)
+                break;
+                
+            case '-':
+                output = sub(a, c)
+                break;
+  
+            case '/':
+                if(a === 0 || c === 0) {
+                    throw "Can't divide by 0!"
+                } else {
+                output = div(a, c)
+                }
+                break;
+        }
+    }
+    catch(e) {
+        console.log("There's and error: ", e)
+    }
+  
+    return output;
+  }
 
-//assign numerical values to the number buttons
-
-//assign mathematical functions to the symbol buttons
-
-//Create function for displaying the numerical values of the buttons on the display
-let buttons = document.querySelectorAll('.number');
-let input = document.getElementById('myInput');
-
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function(e) {
-        input.value = e.target.value;
-    })
-}
+//Create storage variables for display values
+let storedNumber = '';
+let clickedOperator = '';
+let firstNumber = '';
+let result = '';
+currentOperand.textContent = 0;
